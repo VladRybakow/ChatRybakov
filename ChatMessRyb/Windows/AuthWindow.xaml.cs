10a186839f876421e5e1a3ede256ebd4b0494dbc
Владюша Рыбаков <gggame184@gmail.com>
2023-03-11 09:11:28 +0300

    Добавьте файлы проекта.

---
+++ ChatMessRyb/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ChatMessRyb.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChatMessRyb.Windows
+{
+    public partial class AuthWindow : Window
+    {
+        public static ChatDBEntities DB = new ChatDBEntities();
+        public static Employee employee;
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+        private void AuthBTNClick(object sender, RoutedEventArgs e)
+        {
+            if (TBName.Text == "" || TBPassword.Text == "")
+            {
+                MessageBox.Show("Введите логин и пароль!");
+            }
+            foreach (var user in DB.Employee)
+            {
+                if (user.UserName == TBName.Text.Trim())
+                {
+                    if (user.Password == TBPassword.Text.Trim())
+                    {
+                        MessageBox.Show($"Здравствуйте, заместитель директора : {user.Name}");
+
+                        employee = user;
+
+                        ChatDepartTopicWindow main = new ChatDepartTopicWindow();
+                        this.Close();
+                        main.Show();
+                    }
+                }
+            }
+        }
+
+        private void CancelBTNClick(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ ChatMessRyb/Windows/ChatDepartTopicWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ChatMessRyb.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChatMessRyb.Windows
+{
+    public partial class ChatDepartTopicWindow : Window
+    {
+        public static ChatDBEntities DB = new ChatDBEntities();
+        public ChatDepartTopicWindow()
+        {
+            InitializeComponent();
+
+            TBName.Text = AuthWindow.employee.Name;
+            var chatRoom = ((Employee)AuthWindow.employee).Id_Employee;
+            TopicLst.ItemsSource = DB.ChatMessage.Where(x => x.Id_Employee == chatRoom).ToList();
+        }
+    }
+}
